c91b69058084aef0609897e4511acbd8b5cebf37
RarceD <akimbo170@gmail.com>
2021-01-18 10:04:11 +0100

    save keys of clients and check if they have permisions to post to specific uuid

---
+++ Apikey.py
@@ -0,0 +1,73 @@
+import secrets
+import json
+
+# I have each uuid associated with each client
+## UUID : CLIENT
+
+
+# This is a data class to store in a structured way:
+class AssociationUuidClient(object):
+    def __init__(self, client, uuid):
+        self.client = client
+        self.uuid = []
+        # It does not matter if I received a list or a simple string:
+        if isinstance(uuid, list):
+            for u in uuid:
+                self.uuid.append(u)
+        else:
+            self.uuid.append(uuid)
+
+    def __repr__(self):
+        data_response = str(self.client) + " - " + str(self.uuid)
+        return data_response
+
+# I store all the clients and keys and check if the post is correct
+
+
+class Apikey (object):
+    def __init__(self):
+        # Store the association uuid inside this class
+        self.association_uuid_client = []
+        # Load this association from a file first
+
+    def load_asssociation(self, file):
+        try:
+            with open(file) as json_file:
+                data = json.load(json_file)
+                for r in data['requests']:
+                    client_uuid = AssociationUuidClient(r['client'], r['uuid'])
+                    self.association_uuid_client.append(client_uuid)
+        except:
+            raise Exception("Unable to load the file on the route:  " + file)
+
+    def generate_key(self, length):
+        return secrets.token_urlsafe(length)
+
+    def check_if_associated(self, uuid, client):
+        # This is not very eficient but for now the number of clients is not very high:
+        for c in self.association_uuid_client:
+            if client in c.client:
+                print("The client exist")
+                for u in c.uuid:
+                    if uuid == u:
+                        print("Match the uuid")
+                        return True
+                else:
+                    print("This client does not have this uuid")
+                    return False
+        else:
+            return False
+
+
+def test_api_key():
+    data_keys = Apikey()
+    data_keys.load_asssociation('data/uuid_client.json')
+    for d in data_keys.association_uuid_client:
+        print(d)
+    random_client = ["hola", "acua_client"]
+    random_uuid = "Xxfs9g510gvBXP7YGjRGMg"
+    for c in random_client:
+        print(data_keys.check_if_associated(uuid=random_uuid, client=c))
+
+
+# test_api_key()
