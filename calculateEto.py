b2d81739bfe9dcba40f28c0800da68bcce1a6be3
RarceD <akimbo170@gmail.com>
2021-01-26 16:32:20 +0100

    integrate the data between classes

--- calculateEto.py
+++ calculateEto.py
@@ -1,5 +1,7 @@
 import math
 from requestAemet import RequestAemet
+import datetime
+
 
 class calculateEto(object):
     """
@@ -27,20 +29,39 @@ class calculateEto(object):
         self.height = 800.0  # in meters
         self.hum_max = 0.0
         self.hum_min = 0.0
+        self.hum_med = 0.0
         self.c_p = 1.102*10**(-3)
-        self.presure_atm = 0.0
-        self.latitude = 0
-        self.days = 0
+        self.height = 860.0  # in meters
+
+        self.presure_atm = 1020.0
+        self.latitude = 42.34
+        self.days = self.get_julian(datetime.datetime.now())
         self.wind_speed = 8  # in km/hour
+
+    def __repr__(self):
+        data = ""
+        data += "Tmed - Tmax - Tmin: " + \
+            str(self.t_med)+" ! " + str(self.t_max) + \
+            " ! " + str(self.t_min)+"\n"
+        data += "Hmed - Hmax - Hmin: " + \
+            str(self.hum_med)+" ! " + str(self.hum_max) + \
+            " ! " + str(self.hum_min)+"\n"
+        data += "wind speed: " + str(self.wind_speed)+"\n"
+        data += "Height: " + str(self.height)+"\n"
+        return data
+
+    def get_data(self):
+        weather_data = RequestAemet()
+        self.t_max, self.t_min, self.t_med, self.hum_max, self.hum_min, self.wind_speed = weather_data.get_weather_params()
 
     def calc_eto(self):
         # get rad solar from api:
         self.rad_solar = self.get_rad_solar()
         self.rad_net_short = (1-self.albedo)*self.rad_solar
         self.rad_net_wave_no_cloud = (
-            0.75 + (2*self.height/100000))*self.calc_radiation_extra(self.days, self.latitude)
+            0.75 + (2*self.height/100000))*self.calc_radiation_extra()
         presure_vap, e_s = self.calc_preasure()
-        self.rad_net_long = (4.903*10**(-9.0/2.0))*1.35*((self.rad_solar/self.rad_solar_0)-0.35)*(
+        self.rad_net_long = (4.903*10**(-9/2))*1.35*((self.rad_solar/self.rad_solar_0)-0.35)*(
             0.34-0.14*presure_vap**0.5)*((self.t_max+273.0)**4 + (self.t_min+273.0)**4)
         self.rad_net = self.rad_net_long - self.rad_net_short
 
@@ -50,7 +71,8 @@ class calculateEto(object):
         self.wind_speed /= 3.6  # Convert to m/s
 
         # The last operation to get the ET_0:
-        num = delta*(self.rad_net - thermal_flow)+((gamma*900*self.wind_speed) / (self.t_med+273.16))*lambda_vap*(e_s-presure_vap)
+        num = delta*(self.rad_net - thermal_flow)+((gamma*900*self.wind_speed) /
+                                                   (self.t_med+273.16))*lambda_vap*(e_s-presure_vap)
         den = delta+lambda_vap*(1+0.34*self.wind_speed)
         et_0 = 1/lambda_vap * num/den
         # print(self.wind_speed)
@@ -59,18 +81,19 @@ class calculateEto(object):
     def get_rad_solar(self):
         return 0.1
 
-    def calc_radiation_extra(self, days, latitude):
+    def calc_radiation_extra(self):
         # days are on julian days
-        distance_earth_sun = 1 + 0.033 * math.cos(((2*math.pi)/365)*days)
+        distance_earth_sun = 1 + 0.033 * math.cos(((2*math.pi)/365)*self.days)
         # Latitude and logitude on radians:
-        latitude = math.radians(latitude)
+        #Convert latitude in º to radians
+        self.latitude = math.radians(self.latitude)
         # Solar declination:
-        declination = 0.409*math.sin((2*math.pi)/365.0 * days - 1.39)
+        declination = 0.409*math.sin((2*math.pi)/365.0 * self.days - 1.39)
         # Compute solar angle:
-        solar_angle = math.acos(-math.tan(latitude)*math.tan(declination))
+        solar_angle = math.acos(-math.tan(self.latitude)*math.tan(declination))
 
-        ra = (24.0*60.0*0.082)/math.pi * distance_earth_sun * distance_earth_sun*(solar_angle*math.sin(latitude)
-                                                                                  * math.sin(declination) + math.cos(latitude)*math.cos(declination)*math.sin(solar_angle))
+        ra = (24.0*60.0*0.082)/math.pi * distance_earth_sun * distance_earth_sun*(solar_angle*math.sin(self.latitude)
+                                                                                  * math.sin(declination) + math.cos(self.latitude)*math.cos(declination)*math.sin(solar_angle))
         return ra
 
     def calc_preasure(self):
@@ -90,13 +113,17 @@ class calculateEto(object):
         e_med = 0.6108*math.e**((17.27*t)/(t+237.3))
         return (4098*e_med) / ((self.t_max+self.t_min)/237.3)**2
 
-# r = RequestAemet().weather_today_tomorrow()
-# print(r)
-r = RequestAemet().get_weather_params()
-print(r)
+    def get_julian(self, date):
+        # Substract the current day to the first day of the year:
+        first_day_year = datetime.datetime(date.year, 1, 1)
+        julian_day = (date-first_day_year).days + 1
+        return julian_day
+
 
 calc = calculateEto()
-
+print(calc)
+calc.get_data()
+print(calc)
 print(calc.calc_eto())
 # a = RequestAemet()
 # print(a)
--- requestAemet.py
+++ requestAemet.py
@@ -1,25 +1,29 @@
 from credential import AEMET_KEY
-import requests 
+import requests
 import json
+import datetime
+import math
+
 
 class RequestAemet(object):
     def __init__(self):
-        self.t_med = 15.0
+        # get them from Aemet:
+        self.t_med = 0.0
         self.t_medprevious = 0.0
-        self.t_max = 20.0
-        self.t_min = 10.0
-        self.rad_net = 0.0
-        self.rad_solar = 0.0  # obtein from api
-        self.rad_solar_0 = 0.1
-        self.height = 800.0  # in meters
+        self.t_max = 0.0
+        self.t_min = 0.0
         self.hum_max = 0.0
         self.hum_min = 0.0
+        self.hum_med = 0.0
+        self.wind_speed = 0  # in km/hour
+
+        self.rad_net = 0.0
+        self.rad_solar = 0.0  # still do not know hoy to get it
+        self.rad_solar_0 = 0.1
+
+        self.height = 860.0  # in meters
         self.presure_atm = 0.0
-        self.latitude = 0
-        self.days = 0
-        self.wind_speed = 8  # in km/hour
-    def __repr__(self):
-        return self.t_med
+        self.latitude = 42.34106
 
     def get_weather_params(self):
         url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones/"
@@ -31,10 +35,12 @@ class RequestAemet(object):
         # print(json_response)
         response = requests.request('GET', json_response['datos'])
         json_response = json.loads(response.text)
+        # print(json.dumps(json_response, indent=5))
         # print(json.dumps(json_response['prediccion']['dia'][0]['probPrecipitacion'], indent=5))
         temp_max_min = []
         hum_max_min = []
         t_med = []
+        wind_speed = []
         for prov in json_response:
             for i in range(1):
                 data_temp = prov['prediccion']['dia'][i]['temperatura']
@@ -44,23 +50,31 @@ class RequestAemet(object):
                 data_temp = prov['prediccion']['dia'][i]['temperatura']['dato']
                 for d in data_temp:
                     t_med.append(d['value'])
-        # print(json.dumps(prov['prediccion']['dia'][1], indent=4))
-        print(t_med)
+                data_temp = prov['prediccion']['dia'][i]['viento']
+                for v in data_temp:
+                    wind_speed.append(v['velocidad'])
+        # print(json.dumps(prov['prediccion']['dia'][0], indent=4))
         self.t_max = temp_max_min[0][0]
-        self.t_min =temp_max_min[0][1]
-        self.hum_max = temp_max_min[0][0]
+        self.t_min = temp_max_min[0][1]
+        self.t_med = sum(t_med)/len(t_med)
+        self.hum_max = hum_max_min[0][0]
         self.hum_min = hum_max_min[0][1]
-        return self.t_max, self.t_min, self.hum_max, self.hum_min
+        self.wind_speed = sum(wind_speed)/len(wind_speed)
+        return (self.t_max,
+                self.t_min,
+                self.t_med,
+                self.hum_max,
+                self.hum_min,
+                self.wind_speed)
 
-
-    def weather_radars(self):
-        url = "https://opendata.aemet.es/opendata/api/red/radar/nacional"  # for burgos
+    def get_open_data(self):
+        url = "https://opendata.aemet.es/opendata/api/mapasygraficos/analisis"  # for burgos
         querystring = {"api_key": AEMET_KEY}
         response = requests.request(
             "GET", url, headers={'cache-control': "no-cache"}, params=querystring)
+
         json_response = json.loads(response.text)
+        print(json.dumps(json_response, indent=4))
         print(json_response['datos'])
         response = requests.request('GET', json_response['datos'])
         # json_response = json.loads(response.text)
-        
-        print(response.text)
