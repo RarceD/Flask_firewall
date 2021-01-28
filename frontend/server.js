187f8e25719c6cb1bfc384e9093ce48a04f48ad1
RarceD <akimbo170@gmail.com>
2021-01-28 17:21:44 +0100

    create web interface to make http get

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,7 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 app.get('/calculate', (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/calculate.html"));
+  res.sendFile(path.join(__dirname + "/calculate.html"));
   // res.render("/calculate.html");
  });
 var server = app.listen(80, function () {
