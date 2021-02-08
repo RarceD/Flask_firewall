6103874fc2dcffc85b1a66fb6ecfbadc3e46c7ea
RarceD <akimbo170@gmail.com>
2021-02-08 16:08:54 +0100

    create the solve function and prevent not loading all the json params

--- cropstages.py
+++ cropstages.py
@@ -10,31 +10,38 @@ class Cropstages(object):
         self.depths = []
 
     def load_file(self, file):
-        with open(file) as json_file:
-            data = json.load(json_file)
-            # I get the DD/MM/YYYY format so I convert to datetime:
-            self.seedtime = list(data['seedtime'].split("/"))
-            self.seedtime = datetime.datetime(
-                2000 + int(self.seedtime[2]), int(self.seedtime[1]), int(self.seedtime[0]))
-            # Get the julians days in a list:
-            julian_init_day = self._get_julian(self.seedtime)
-            self.stage_days = data['stage_days']
-            days_julians = []
-            previous_s = 0
-            for s in self.stage_days:
-                previous_s += s
-                days_julians.append(self._get_julian(
-                    self.seedtime + timedelta(days=previous_s)))
-            self.stage_days = days_julians.copy()
-            # The kc provided by the client:
-            self.kc = data['kc']
-            self.depths_radicualar = data['depths_radicualar']
-            self.depths_suelo = data['depths_suelo']
-            self.stone_percentage = data['stone_percentage']
-            self.precipitation_day = data['precipitation_day']
-            self.efective_precipitation_day = data['efective_precipitation_day']
-            self.iHD_initial = data['iHD_initial']
-            self.radiocular_ = data['radiocular_']
+        try:
+            with open(file) as json_file:
+                data = json.load(json_file)
+                # I get the DD/MM/YYYY format so I convert to datetime:
+                self.seedtime = list(data['seedtime'].split("/"))
+                self.seedtime = datetime.datetime(
+                    2000 + int(self.seedtime[2]), int(self.seedtime[1]), int(self.seedtime[0]))
+                # Get the julians days in a list:
+                self.seedtime = self._get_julian(self.seedtime)
+                self.stage_days = data['stage_days']
+                days_julians = []
+                previous_s = 0
+                for s in self.stage_days:
+                    previous_s += s
+                    days_julians.append(self._get_julian(
+                        self.seedtime + timedelta(days=previous_s)))
+                self.stage_days = days_julians.copy()
+                # The kc provided by the client:
+                self.kc = data['kc']
+                self.depths_radicualar = data['depths_radicualar']
+                self.depths_suelo = data['depths_suelo']
+                self.stone_percentage = data['stone_percentage']
+                self.precipitation_day = data['precipitation_day']
+                self.efective_precipitation_day = data['efective_precipitation_day']
+                self.iHD_initial = data['iHD_initial']
+                self.radiocular_ = data['radiocular_']
+                self.et0 = data['et0']
+                self.cred_rad = data['cred_rad']
+                self.nap = data['nap']
+                return True
+        except:
+            return False
 
     def __repr__(self):
         data_inside = ""
@@ -45,17 +52,36 @@ class Cropstages(object):
             str(self.depths_radicualar) + "\n"
         return data_inside
 
-    def _calc_eta(self):
+    def solve(self):
         # The first eta is the first day etc.
+        eta = 0
+        efective_precipitation = self.precipitation_day * \
+            self.efective_precipitation_day/100
+        fc = 0  # i don't have the fc
+        water_balance_rad = self.iHD_initial / \
+            (100*fc*(1-self.stone_percentage)*1000*self.depths_radicualar[0])
+        wp = 0  # i don't have it and don't even know what it is.
+        pr = 0
+        cred_rad = 0
+        eta_num = water_balance_rad+efective_precipitation + \
+            self.precipitation_day+cred_rad * \
+            (wp*(1-self.stone_percentage)*pr*1000)
 
+        eta_den = (1-self.nap)/((1-self.stone_percentage)*(fc-wp)*pr*1000)
         # The following ta
-        pass
+        return eta_num/eta_den
 
-    def _calc_evo(self):
-        if self.seedtime < stage_days[0]:
-            return 0
+    def _calc_evo(self, stage, prev_evo):
+        # TODO: Let's make any sense---
+        if stage == 0:
+            if self.seedtime < stage_days[0]:
+                return 0
+            else:
+                return stage_days[0]
+        if stage == 1:
+            return prev_evo
         elif self.seedtime < stage_days[1]:
-            return stage_days[0]
+            return
         elif self.seedtime < stage_days[1]:
             pass
 
@@ -67,5 +93,5 @@ class Cropstages(object):
 
 
 cropstages = Cropstages()
-cropstages.load_file('data/cropstages.json')
-print(cropstages)
+if (cropstages.load_file('data/cropstages.json')):
+    print(cropstages)
