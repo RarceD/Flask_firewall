9c59d4530893e767e05e119bdee03b4d3eef4d4d
RarceD <akimbo170@gmail.com>
2021-02-10 12:19:44 +0100

    add client to uuid

--- databaseinteractions.py
+++ databaseinteractions.py
@@ -15,11 +15,17 @@ class DatabaseInteractions(object):
             database=DB_DATA['database']
         )
         self.mycursor = self.mydb.cursor()
+        self.mycursor.execute("SELECT * FROM rarced.clients;")
+        self.number_rows = 0
+        for _ in self.mycursor:
+            number_rows+=1
+        print(number_rows)
+
 
     def add_client(self, name):
-        sql = "INSERT rarced.clients (id, client_name, uuid, asociated_uuid) VALUES (%s,%s,%s,%s)"
+        sql = "INSERT rarced.clients (client_name, uuid, asociated_uuid) VALUES (%s,%s,%s)"
         # generate the random api key to the new client
-        val = (6, name, Apikey().generate_key(32), 0)
+        val = (name, Apikey().generate_key(32), 0)
         self.mycursor.execute(sql, val)
         self.mydb.commit()
 
@@ -29,11 +35,15 @@ class DatabaseInteractions(object):
         self.mycursor.execute(sql)
         self.mydb.commit()
 
+    def add_uuid_to_client(self, uuid, client):
+        sql = "UPDATE rarced.clients SET client_name=\""+str(uuid)+"\" WHERE uuid=\="+str(uuid)+"\""
+        self.mycursor.execute(sql)
+        self.mydb.commit()
 
-# sql = DatabaseInteractions()
+
+sql = DatabaseInteractions()
 # sql.add_client("paquito")
 # sql.delete_client("paquito")
+# sql.add_uuid_to_client(client="paquito", uuid="uuid-inventado")
+
 # For showing all the databases asociated:
-# mycursor.execute("SHOW DATABASES")
-# for x in mycursor:
-#     print(x)
